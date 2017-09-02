3a9a918fbbe51c8b5ad534ef2a4a3cee9858cf7f
Ryan Moore <ryan.moore@mx.com>
2017-09-02 11:33:14 -0600

    playing with code reloading

--- adding_webpack/src/index.js
+++ adding_webpack/src/index.js
@@ -11,7 +11,7 @@ function initGl(gl) {
   //defaults to 0, 0, canvas width, canvas height, so this is not necessary unless the canvas will change size
   gl.viewport(0, 0, canvas.width, canvas.height);
 
-  gl.clearColor(1, 0, 0, 1); //r, g, b, alpha values
+  gl.clearColor(1, 1, 0, 1); //r, g, b, alpha values
 }
 
 function draw(gl) {
