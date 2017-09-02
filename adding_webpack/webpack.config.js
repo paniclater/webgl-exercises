03533ff0c94b202997446bf78d1a4178b24e4b43
Ryan Moore <ryan.moore@mx.com>
2017-09-02 11:33:35 -0600

    adding hot module replacement in dev mode

--- adding_webpack/webpack.config.js
+++ adding_webpack/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 
 module.exports = {
@@ -9,11 +10,16 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
   devtool: 'inline-source-map',
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Learning WebGL'
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin()
   ]
 };
