ae4fb668ddca05d5eab5050243dd19cc7f6b2df4
Ryan Moore <ryan.moore@mx.com>
2017-09-02 10:41:28 -0600

    passing in gl instead of just establishing it as a global

--- setting-up/index.js
+++ setting-up/index.js
@@ -1,21 +1,20 @@
-console.log('setting up webgl');
-let gl;
+const gl = document
+  .getElementById('canvas')
+  .getContext('webgl');
 
-initGl();
-draw();
+initGl(gl);
+draw(gl);
 
-function initGl() {
-  const canvas = document.getElementById('canvas');
-  gl = canvas.getContext('webgl');
+function initGl(gl) {
   // set the viewport of the webl context
   //x of lower left corner, y of lower left corner, width and height
   //defaults to 0, 0, canvas width, canvas height, so this is not necessary unless the canvas will change size
   gl.viewport(0, 0, canvas.width, canvas.height);
 
-  gl.clearColor(1, 0, 0, 1); //r, g, b, opacity values
+  gl.clearColor(1, 0, 0, 1); //r, g, b, alpha values
 }
 
-function draw() {
+function draw(gl) {
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
