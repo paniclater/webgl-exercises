0a565bf3dfc1f53da66fd2bc17aed82a430db6f6
Ryan Moore <ryan.moore@mx.com>
2017-09-02 11:18:05 -0600

    webpack config for html generation and cleaning dist

--- adding_webpack/webpack.config.js
+++ adding_webpack/webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Learning WebGL'
+    })
+  ]
 };
