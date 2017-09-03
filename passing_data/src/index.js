9985a14f675fb4910ed8aeb0b2849c86c27ce321
Ryan Moore <ryan.moore@mx.com>
2017-09-02 20:37:31 -0600

    no more static point location, size or color

--- passing_data/src/index.js
+++ passing_data/src/index.js
@@ -3,8 +3,8 @@ const { IO } = require('monet');
 //These are written in Open GL, a c like language for graphics
 //Be very careful here, everything will run in javascript land if these are invalid
 //But there will be a warning that it is an invalid program
-const vsString = () => "void main(void) { gl_Position = vec4(0.0, 0.0, 0.0, 1.0); gl_PointSize = 100.0; }";
-const fsString = () => "void main(void) { gl_FragColor = vec4(0.25, 1.0, 0.5, 1.0); }";
+const vsString = () => "attribute vec4 coords; attribute float pointSize; void main(void) { gl_Position = coords; gl_PointSize = pointSize; }";
+const fsString = () => "precision mediump float; uniform vec4 color; void main(void) { gl_FragColor = color; }";
 
 const makeVertexShader =
   (gl, program) => () =>
@@ -14,7 +14,8 @@ const makeVertexShader =
       .bind(vertexShader =>
         IO(() => gl.shaderSource(vertexShader, vs))
         .map(() => gl.compileShader(vertexShader))
-        .map(() => gl.attachShader(program, vertexShader))));
+        .map(() => gl.attachShader(program, vertexShader))
+      ));
 
 const makeFragmentShader =
   (gl, program) => () =>
@@ -39,7 +40,12 @@ const app = id =>
         .bind(makeVertexShader(gl, program))
         .bind(makeFragmentShader(gl, program))
         .map(() => gl.linkProgram(program))
+        // must set attributes after program is linked
+        .map(() => gl.vertexAttrib3f(gl.getAttribLocation(program, 'coords'), -0.5, -0.5, 0))
+        .map(() => gl.vertexAttrib1f(gl.getAttribLocation(program, 'pointSize'), 1000))
         .map(() => gl.useProgram(program))
+        //must set uniform after program is in use
+        .map(() => gl.uniform4f(gl.getUniformLocation(program, 'color'), .25, 1, .75, 1))
         .map(() => gl.clearColor(1, 0, 1, 1))
         .map(() => gl.clear(gl.COLOR_BUFFER_BIT))
         .map(() => gl.drawArrays(gl.POINTS, 0,1))
