d208fc876642570c58c1c09edd024dddb3bc0f90
Ryan Moore <ryan.moore@mx.com>
2017-09-03 16:22:26 -0600

    animating points. but had to break out of monad land

--- animating_points/src/index.js
+++ animating_points/src/index.js
@@ -8,7 +8,6 @@ const vsString = () => document.getElementById('vertex-shader').text;
 const fsString = () => document.getElementById('fragment-shader').text;
 
 const vertices = () => new Array(VERTEX_COUNT * 2).fill().map(_ => Math.random() * 2 - 1);
-vertices();
 
 
 //vertex shader handles point location and size
@@ -32,35 +31,57 @@ const makeFragmentShader = (gl, program) => () =>
       .map(() => gl.compileShader(fragmentShader))
       .map(() => gl.attachShader(program, fragmentShader))));
 
-const configureArrayBuffer = gl => coords =>
+const configureArrayBuffer = (gl, vertices) => coords =>
   //bind the buffer
-  IO(() => gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer()))
+  IO(() => {
+    return gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer())
+  })
   //use the coords attribute, three points, float values
   .map(() => gl.vertexAttribPointer(coords, 2, gl.FLOAT, false, 0, 0))
   //use the array buffer, send in our actual coords, use a static strategy to draw (very efficient, not dynamic)
-  .map(() => gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices()), gl.STATIC_DRAW))
+  .map(() => gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW))
   //enable the array with our coords attribute
   .map(() => gl.enableVertexAttribArray(coords))
   //unbind the buffer
-  .map(() => gl.bindBuffer(gl.ARRAY_BUFFER, null))
+//.map(() => gl.bindBuffer(gl.ARRAY_BUFFER, null))
+  .map(() => vertices);
 
 // must set attributes after program is linked
 //must set uniform after program is in use
-const initializeProgram = (gl, program) => () =>
+const initializeProgram = (gl, program) => vertices =>
   IO(() => gl.linkProgram(program))
   .map(() => gl.useProgram(program))
   .map(() => gl.getAttribLocation(program, 'coords'))
-  .bind(configureArrayBuffer(gl))
+  .bind(configureArrayBuffer(gl, vertices))
   .map(() => gl.vertexAttrib1f(gl.getAttribLocation(program, 'pointSize'), 5))
   .map(() => gl.uniform4f(gl.getUniformLocation(program, 'color'), .25, 1, .75, 1))
+  .map(() => vertices);
 
-const draw = gl => () => IO(() => gl.clearColor(1, 0, 1, 1)) //sets the clear color
-  .map(() => gl.clear(gl.COLOR_BUFFER_BIT)) //actually draws the clear color
-//LINES draws lines for each complete vertex pair
-//LINE_STRIP draws lines for each vertex without closing the loop
-//LINE_LOOP draws lines for each vertex and closes the loop
-//TRIANGLES makes a filled triangle
-  .map(() => gl.drawArrays(gl.POINTS, 0, VERTEX_COUNT)) //
+//function draw(gl) {
+//  return function(vertices) {
+//    return IO(() => vertices.map(v => v + Math.random() * 0.01 - 0.005))
+//    .map(vs => gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array(vs)))
+//    .map(() => gl.clearColor(1, 0, 1, 1))
+//    .map(() => gl.clear(gl.COLOR_BUFFER_BIT))
+//    .map(() => gl.drawArrays(gl.POINTS, 0, VERTEX_COUNT))
+//    .map(() => {
+//      return requestAnimationFrame(draw(gl)(vertices).run());
+//    });
+//  }
+//}
+
+const draw = (gl, vertices) => {
+  return function () {
+    const newVs = vertices.map(v => v + Math.random() * 0.01 - 0.005);
+    gl.clearColor(1, 0, 1, 1)
+    gl.clear(gl.COLOR_BUFFER_BIT)
+    gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array(newVs))
+    gl.drawArrays(gl.POINTS, 0, VERTEX_COUNT)
+
+    requestAnimationFrame(draw(gl, newVs));
+  };
+};
+
 
 const app = id =>
   IO(() => document.write(`<canvas id="${id}" height="600" width="600"></canvas>`))
@@ -74,9 +95,13 @@ const app = id =>
         IO(() => program)
         .bind(makeVertexShader(gl, program))
         .bind(makeFragmentShader(gl, program))
+        .map(() => vertices())
         .bind(initializeProgram(gl, program))
-      )
-      .bind(draw(gl))
-    ));
+        .bind(vs => {
+          draw(gl, vs)();
+
+          return IO(() => gl);
+        })
+    )));
 
 app('canvas').run();
